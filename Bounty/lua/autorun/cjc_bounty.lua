adcebb2f18bf37ee195e136c80130f8bbb1f8792
Devocity <chajecraft@hotmail.com>
2017-12-29 21:00:31 +0100

    Done lmao
    fucking end me

--- Bounty/lua/autorun/cjc_bounty.lua
+++ Bounty/lua/autorun/cjc_bounty.lua
@@ -2,7 +2,7 @@
 
 if SERVER then
 	util.AddNetworkString("Bounty_Gained")
-	util.AddNetworkString("Bounty_Collected")
+	util.AddNetworkString("Bounty_Lost")
 	util.AddNetworkString("Bounty_Claimed")
 	local meta = FindMetaTable( 'player' )
 	
@@ -18,12 +18,21 @@ if SERVER then
 		self:SetNWBool("ActiveBounty", true)
 		self:SetNWInt("Bounty", amt + (self:GetBounty() or 0))
 		self.BountyResets = CurTime() + (900 + amt/20)
+		net.Start("Bounty_Gained")
+			net.WriteEntity(self)
+			net.WriteString(tostring(amt))
+		net.Broadcast()
 	end
 	
-	function meta:ResetBounty()
+	function meta:ResetBounty(claimed)
 		self:SetNWBool("ActiveBounty", false)
 		self:SetNWInt("Bounty", nil)
 		self.BountyResets = nil
+		if !claimed then
+			net.Start("Bounty_Lost")
+				net.WriteEntity(self)
+			net.Broadcast()
+		end
 	end
 	
 	BountySystem = BountySystem or {}
@@ -33,15 +42,31 @@ if SERVER then
 		ply:ResetBounty()
 	end
 	
-	function BountySystem.ClaimBounty( bountykeeper, bountyhunter )
-	
-		
-		bountykeeper:ResetBounty()
-		if bountyhunter:Team() == TEAM_BOUNTY then
-			
-		else
-			
+	function BountySystem.Think()
+		for k,ply in pairs(player.GetAll()) do
+			if ply.BountyResets && ply.BountyResets <= CurTime() then
+				ply.BountyResets = nil
+				ply:ResetBounty()
+			end
 		end
+	end
+	hook.Add("Think", "BountySystemThink", BountySystem.Think)
+	
+	function BountySystem.ClaimBounty( bountykeeper, bountyhunter )
+		local amt
+		if bountyhunter:Team() == TEAM_BOUNTY then
+			amt = bountykeeper:GetBounty()
+		else
+			amt = math.Round(bountykeeper:GetBounty()*0.4)
+		end
+		bountyhunter:addMoney(amt)
+		net.Start("Bounty_Claimed")
+			net.WriteEntity(bountyhunter)
+			net.WriteEntity(bountykeeper)
+			net.WriteString(tostring(amt))
+		net.Broadcast()
+		
+		bountykeeper:ResetBounty(true)
 	end
 	
 	function BountySystem.Killed( ply, inflictor, attacker )
@@ -49,15 +74,25 @@ if SERVER then
 		if !attacker:IsPlayer() then return end
 		if !ply:HasBounty() then return end
 		
-		if attacker:Team() == TEAM_BOUNTY then
-			
-		else
-		
-		end
+		BountySystem.ClaimBounty( ply, attacker )
 	end
 	hook.Add("PlayerDeath", "BountySystemKill", BountySystem.Killed)
 end
 
 if CLIENT then
-
+	net.Receive("Bounty_Claimed", function()
+		local ply1 = net.ReadEntity()
+		local ply2 = net.ReadEntity()
+		chat.AddText(Color(255,255,255), "[", Color(255,0,0), "ALERT", Color(255,255,255), "] "..ply1:Nick().." claimed the bounty of "..ply2:Nick().." worth of "..net.ReadString().."CJ$." )
+	end)
+	
+	net.Receive("Bounty_Gained", function()
+		local ply = net.ReadEntity()
+		chat.AddText(Color(255,255,255), "[", Color(255,0,0), "ALERT", Color(255,255,255), "] "..ply:Nick().." gained a bounty of "..net.ReadString().."CJ$.")
+	end)
+	
+	net.Receive("Bounty_Lost", function()
+		local ply = net.ReadEntity()
+		chat.AddText(Color(255,255,255), "[", Color(255,0,0), "ALERT", Color(255,255,255), "] "..ply:Nick().." lost his bounty.")
+	end)
 end
